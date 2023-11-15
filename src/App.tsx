f2a5304e1e7280a5d064c122ed291e21df99f872
wojtek <wswistak@student.agh.edu.pl>
2023-11-15 23:16:21 +0100

    config bs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Button from "react-bootstrap/Button";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <p>Click on the Vite and React logos to learn more</p>
+      <p className="p-4 m-4">
+        <Button variant="primary" onClick={() => setCount(count + 1)}>
+          You clicked {count} times
+        </Button>
+        Cldick on the Vite and React logos to learn more
+      </p>
     </>
   );
 }

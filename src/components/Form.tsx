d723437f7f3a115dfab9275628445efab7a62890
wojtek <wswistak@student.agh.edu.pl>
2023-11-24 21:56:39 +0100

    style form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Form as FormBs } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import { MdAdd } from "react-icons/md";
 
 interface Task {
   task: string;
@@ -19,34 +18,60 @@ const Form: React.FC = () => {
   };
   return (
     <FormBs
-      className="d-flex mt-4 mt-sm-0 "
+      className=" mt-4 mt-sm-0 "
       onSubmit={(e) => {
         e.preventDefault();
       }}
     >
+      <h5 className="">Enter the task </h5>
       <FormBs.Control
         type="text"
-        placeholder="Add new task"
-        className="rounded-0 border-0 text-light custom-input "
+        placeholder="Learn javascript"
+        className="rounded-1 border-0 text-light custom-input mb-4 "
         style={{ backgroundColor: "var(--clr-primary-light)" }}
         onChange={(e) => setInputValue(e.target.value)}
         required
         minLength={4}
       />
-      <div
-        className="px-2 py-1"
-        style={{ backgroundColor: "var(--clr-primary-light)" }}
-      >
-        <Button
-          variant="primary"
-          type="submit"
-          className="rounded-0 font-weight-bold p-0"
-          style={{ backgroundColor: "inherit", border: "none" }}
-          onClick={handleAddTask}
-        >
-          <MdAdd size={"32px"} />
-        </Button>
+      <h5 className="">Select the priority level of the task </h5>
+      <div className="d-flex align-items-center mb-1 ">
+        <FormBs.Check
+          type="radio"
+          name="priority"
+          id="high"
+          className="radio-custom"
+          required
+        />
+        <FormBs.Label htmlFor="high">High</FormBs.Label>
       </div>
+      <div className="d-flex align-items-center mb-1">
+        <FormBs.Check
+          type="radio"
+          name="priority"
+          id="medium"
+          className="radio-custom"
+          required
+        />
+        <FormBs.Label htmlFor="medium">Medium</FormBs.Label>
+      </div>
+      <div className="d-flex align-items-center mb-1">
+        <FormBs.Check
+          type="radio"
+          name="priority"
+          id="low"
+          className="radio-custom"
+          required
+        />
+        <FormBs.Label htmlFor="low">Low</FormBs.Label>
+      </div>
+
+      <Button
+        type="submit"
+        className="fw-semibold btn-custom rounded-4 px-4 py-2 mt-4"
+        onClick={handleAddTask}
+      >
+        Add task
+      </Button>
     </FormBs>
   );
 };

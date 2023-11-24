80b76668758aa3d08789e75a747e4438848c0c8b
wojtek <wswistak@student.agh.edu.pl>
2023-11-24 21:53:39 +0100

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import AppLayout from "./Layouts/AppLayout";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Form from "./components/Form";
+import AddTask from "./pages/AddTask";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<AppLayout />}>
-          <Route index element={<Form />} />
-          <Route path="add-task" element={<Form />} />
+          <Route index element={<AddTask />} />
+          <Route path="add-task" element={<AddTask />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/AddTask.tsx
@@ -0,0 +1,7 @@
+import Form from "../components/Form";
+
+const AddTask = () => {
+  return <Form />;
+};
+
+export default AddTask;

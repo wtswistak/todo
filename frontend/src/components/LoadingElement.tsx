6bd4ac03c42f750de8fef430fe9aef989636759d
wojtek <wswistak@student.agh.edu.pl>
2023-12-01 23:29:43 +0100

    loading element

---
+++ frontend/src/components/LoadingElement.tsx
@@ -0,0 +1,12 @@
+const LoadingElement = () => {
+  return (
+    <div className="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default LoadingElement;

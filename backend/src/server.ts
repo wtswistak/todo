81da89770b6c5342c1ffd43241c3ba1a15af796e
wojtek <wswistak@student.agh.edu.pl>
2023-12-02 19:39:14 +0100

    add auth funcs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,52 @@
 import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import { addUser, loginUser, UserError } from "./services/userService";
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+
 app.get("/", (req: Request, res: Response) => {
+  console.log("work");
   res.send("work");
+});
+
+app.post("/sign-up", async (req: Request, res: Response) => {
+  const { username, email, password } = req.body;
+
+  try {
+    await addUser(username, email, password);
+    res.status(201).json({ message: "Register user success" });
+  } catch (error) {
+    console.error("Register problem:", error);
+    if (error instanceof UserError) {
+      res.status(400).json({
+        message: "User with this email or username already exists",
+        error: error.message,
+      });
+    } else {
+      res.status(500).json({ message: "Register problem" });
+    }
+  }
+});
+app.post("/login", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  console.log(username, password);
+  try {
+    await loginUser(username, password);
+    res.status(200).json({ message: "Login user success" });
+  } catch (error) {
+    console.error("Login problem:", error);
+    if (error instanceof UserError) {
+      res.status(401).json({
+        message: "Invalid username or password",
+        error: error.message,
+      });
+    } else {
+      res.status(500).json({ message: "Login problem", error: error });
+    }
+  }
 });
 
 app.listen(port, () => {
---
+++ backend/src/services/userService.ts
@@ -0,0 +1,75 @@
+import { PrismaClient, User } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const bcrypt = require("bcrypt");
+
+export class UserError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "UserError";
+  }
+}
+
+export async function addUser(
+  username: string,
+  email: string,
+  password: string
+): Promise<void> {
+  try {
+    const usernameExist = await prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+    });
+
+    const emailExist = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (usernameExist || emailExist) {
+      throw new UserError("Email or username already exists");
+    }
+    const passwordCrypt = await bcrypt.hash(password, 10);
+    await prisma.user.create({
+      data: {
+        username: username,
+        email: email,
+        password: passwordCrypt,
+      },
+    });
+
+    console.log("User added");
+  } catch (error) {
+    console.error("Problem during adding new user", error);
+    throw error;
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+export async function loginUser(
+  username: string,
+  password: string
+): Promise<void> {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+    });
+    if (!user) {
+      throw new UserError("Username not found");
+    }
+    const passwordCrypt = await bcrypt.compare(password, user.password);
+    if (!passwordCrypt) {
+      throw new UserError("Password is incorrect");
+    }
+    console.log("Login success");
+  } catch (error) {
+    console.error("Problem during login", error);
+    throw error;
+  } finally {
+    await prisma.$disconnect();
+  }
+}

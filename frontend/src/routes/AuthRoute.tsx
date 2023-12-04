2da9e171f5cf337cddb17cd69e652953faef8a64
wojtek <wswistak@student.agh.edu.pl>
2023-12-04 21:39:05 +0100

    add auth route

---
+++ frontend/src/routes/AuthRoute.tsx
@@ -0,0 +1,18 @@
+import { useSelector } from "react-redux";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import { selectUser } from "../state/userSlice";
+
+interface AuthRouteProps {
+  children?: React.ReactNode;
+}
+const AuthRoute: React.FC<AuthRouteProps> = ({ children }) => {
+  const user = useSelector(selectUser);
+
+  if (!user) {
+    return <Navigate to="/user/login" />;
+  }
+
+  return <Routes>{children}</Routes>;
+};
+
+export default AuthRoute;

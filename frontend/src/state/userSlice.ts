655b6a2203b643b8444f414aa1b96ece56893c56
wojtek <wswistak@student.agh.edu.pl>
2023-12-09 21:26:38 +0100

    add id

--- frontend/src/state/userSlice.ts
+++ frontend/src/state/userSlice.ts
@@ -1,10 +1,12 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface UserState {
+  id: number | null;
   username: string | null;
 }
 
 const initialState: UserState = {
+  id: null,
   username: null,
 };
 
@@ -12,10 +14,15 @@ const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setUser: (state, action: PayloadAction<string>) => {
-      state.username = action.payload;
+    setUser: (
+      state,
+      action: PayloadAction<{ id: number; username: string }>
+    ) => {
+      state.id = action.payload.id;
+      state.username = action.payload.username;
     },
     clearUser: (state) => {
+      state.id = null;
       state.username = null;
     },
   },
@@ -24,4 +31,4 @@ const userSlice = createSlice({
 export default userSlice.reducer;
 export const { setUser, clearUser } = userSlice.actions;
 
-export const selectUser = (state: { user: UserState }) => state.user.username;
+export const selectUser = (state: { user: UserState }) => state.user;

7967e0c347f45ca2f48306a707f0802ad64d6650
wojtek <wswistak@student.agh.edu.pl>
2023-12-10 20:49:17 +0100

    delete temp data, change display

--- frontend/src/components/TodoList/List.tsx
+++ frontend/src/components/TodoList/List.tsx
@@ -1,73 +1,20 @@
-import { useState } from "react";
 import ListItem from "./ListItem";
-interface Task {
-  id: number;
-  task: string;
-  priority: string;
-  completed: boolean;
-}
+
 interface ListProps {
-  type: string;
   isVisible: boolean;
+  todos: ITodo[];
 }
 
-const List: React.FC<ListProps> = ({ type, isVisible }) => {
-  const [tasks, setTasks] = useState<Task[]>([
-    {
-      id: 1,
-      task: "Learn React Learn React Learn React",
-      priority: "high",
-      completed: false,
-    },
-    {
-      id: 2,
-      task: "Learn Typescript",
-      priority: "medium",
-      completed: false,
-    },
-    {
-      id: 3,
-      task: "Learn Redux",
-      priority: "low",
-      completed: false,
-    },
-    {
-      id: 4,
-      task: "Learn Redux",
-      priority: "low",
-      completed: true,
-    },
-    {
-      id: 5,
-      task: "Learn Redux",
-      priority: "low",
-      completed: true,
-    },
-    {
-      id: 6,
-      task: "Learn Redux",
-      priority: "low",
-      completed: true,
-    },
-    {
-      id: 7,
-      task: "Learn Redux",
-      priority: "low",
-      completed: true,
-    },
-  ]);
-
+const List: React.FC<ListProps> = ({ isVisible, todos }) => {
   return (
     <ul
       className={`${
         isVisible ? "d-flex" : "d-none"
       } flex-column gap-2 px-0 mb-4 overflow-hidden `}
     >
-      {tasks
-        .filter((task) => (type === "todo" ? !task.completed : task.completed))
-        .map((task) => {
-          return <ListItem task={task} key={task.id} />;
-        })}
+      {todos?.map((todo) => {
+        return <ListItem todo={todo} key={todo.id} />;
+      })}
     </ul>
   );
 };

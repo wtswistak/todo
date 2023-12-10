b70884b64ee3e196b043651bcf0159da8f19f5c5
wojtek <wswistak@student.agh.edu.pl>
2023-12-10 20:46:42 +0100

    change auth func, add back btn

--- frontend/src/components/Auth/AuthForm.tsx
+++ frontend/src/components/Auth/AuthForm.tsx
@@ -4,68 +4,79 @@ import AuthInput from "../Auth/AuthInput";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { setUser } from "../../state/userSlice";
+import config from "../../config";
+import useFetch from "../../hooks/useFetch";
+import Message from "../Message";
+import { IoArrowBack } from "react-icons/io5";
+
 interface AuthFormProps {
   type: string;
 }
 
 const AuthForm: React.FC<AuthFormProps> = ({ type }) => {
   const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [username, setUsername] = useState<string>("");
-  const [errorMessage, setErrorMessage] = useState<string>("");
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [password, setPassword] = useState<string>("test123");
+  const [username, setUsername] = useState<string>("test123");
+  const [message, setMessage] = useState<string>("");
+  const { handleFetch, isLoading } = useFetch();
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleAuth = async (type: string) => {
-    setIsLoading(true);
-    try {
-      let endpoint = "http://localhost:3000/";
-      let body = {};
+    let body = {};
+    let endpoint = config.SERVER_URL + "auth/";
 
-      if (type === "login") {
-        endpoint += "login";
-        body = { username: username, password: password };
-      } else if (type === "signup") {
-        endpoint += "sign-up";
-        body = { email: email, password: password, username: username };
-      }
+    if (type === "login") {
+      endpoint += "login";
+      body = { username, password };
+    }
+    if (type === "signup") {
+      endpoint += "sign-up";
+      body = { email, password, username };
+    }
 
-      const res = await fetch(endpoint, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-      });
-      const data = await res.json();
-      setErrorMessage(data.message);
-      if (type === "login" && res.ok) {
-        dispatch(setUser(username));
-        localStorage.setItem("token", data.token);
-      }
-
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    } finally {
-      if (type === "login") navigate("/todo/add");
-      // if (type === "signup") navigate("/user/login");
+    const { data, response, error } = await handleFetch(
+      endpoint,
+      "POST",
+      {},
+      body
+    );
+    setMessage(data?.message);
+    if (!response?.ok) {
+      console.log(error);
+      return null;
+    }
+    if (type === "login") {
+      dispatch(setUser({ id: data.userId, username: username }));
+      localStorage.setItem("token", data.token);
+      navigate("/todos/add");
+    }
+    if (type === "signup") {
+      // navigate("/user/login");
       setPassword("");
       setEmail("");
       setUsername("");
-      setIsLoading(false);
     }
   };
   return (
     <div
-      className="container p-0 mx-auto my-sm-5"
+      className="container p-0 mx-auto my-sm-5 auth-form"
       style={{ maxWidth: "340px" }}
     >
-      <h5 className="text-center  ">
-        {type.charAt(0).toUpperCase() + type.slice(1)}
-      </h5>
+      <div className="container position-relative ">
+        <h5 className="text-center  ">
+          {type.charAt(0).toUpperCase() + type.slice(1)}
+        </h5>
+        {type === "signup" && (
+          <IoArrowBack
+            size={24}
+            className="position-absolute end-0 top-0 me-1"
+            style={{ cursor: "pointer" }}
+            onClick={() => navigate("/auth/login")}
+          />
+        )}
+      </div>
       <form
         onSubmit={(e) => {
           e.preventDefault();
@@ -90,16 +101,12 @@ const AuthForm: React.FC<AuthFormProps> = ({ type }) => {
         {type === "login" && (
           <div className="d-flex mt-1">
             <p className="font-sm">You don't have account?</p>
-            <Link to={"/user/sign-up"} className="ms-1 font-sm sign-up-link">
+            <Link to={"/auth/sign-up"} className="ms-1 font-sm sign-up-link">
               Sign up
             </Link>
           </div>
         )}
-        {errorMessage && (
-          <div className="d-flex mt-1">
-            <p className="font-sm font-alert">{errorMessage}</p>
-          </div>
-        )}
+        <Message message={message} />
       </form>
     </div>
   );

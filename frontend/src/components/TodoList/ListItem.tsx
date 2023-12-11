33ca0b79688a7a818fcf63b6e97631226e467a8f
wojtek <wswistak@student.agh.edu.pl>
2023-12-11 23:25:08 +0100

    move interface to another file

--- frontend/src/components/TodoList/ListItem.tsx
+++ frontend/src/components/TodoList/ListItem.tsx
@@ -1,35 +1,25 @@
 import React from "react";
 import { IoMdTrash, IoMdCheckboxOutline } from "react-icons/io";
 
-interface Task {
-  id: number;
-  task: string;
-  priority: string;
-  completed: boolean;
-}
-
-const ListItem: React.FC<{ task: Task }> = ({ task }) => {
+const ListItem: React.FC<{ todo: ITodo }> = ({ todo }) => {
   return (
-    <li
-      key={task.id}
-      className="task-box d-flex py-2 px-3 rounded-1 align-items-center justify-content-between "
-    >
+    <li className="task-box d-flex py-2 px-3 rounded-1 align-items-center justify-content-between ">
       <div className="d-flex align-items-center">
         <span
           className={`me-2 priority ${
-            task.priority === "high"
+            todo.priority === "high"
               ? "priority-high"
-              : task.priority === "low"
+              : todo.priority === "low"
               ? "priority-low"
               : "priority-medium"
           } `}
         ></span>
-        <p>{task.task}</p>
+        <p>{todo.title}</p>
       </div>
       <div className="d-flex gap-1 align-items-center">
         <IoMdTrash className="task-icon" size={26} title="delete" />
 
-        {!task.completed && (
+        {!todo.completed && (
           <IoMdCheckboxOutline
             className="task-icon"
             size={26}

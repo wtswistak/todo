544c30084021c4eded375d0b77a4c5c35c96a97a
wojtek <wswistak@student.agh.edu.pl>
2023-12-12 23:26:29 +0100

    fetch api fun

---
+++ frontend/src/hooks/useFetch.ts
@@ -0,0 +1,45 @@
+import { useDispatch } from "react-redux";
+import { startLoading, stopLoading } from "../state/userSlice";
+
+const useFetch = () => {
+  const dispatch = useDispatch();
+
+  const handleFetch = async (
+    endpoint: string,
+    method: string,
+    headers: Record<string, string> = {},
+    body?: Record<string, any>
+  ) => {
+    try {
+      const controller = new AbortController();
+      const signal = controller.signal;
+
+      dispatch(startLoading());
+      const options: Record<string, any> = {
+        method,
+        headers: {
+          "Content-Type": "application/json",
+          ...headers,
+        },
+        signal,
+      };
+      if (body && method !== "GET" && method !== "HEAD") {
+        options.body = JSON.stringify(body);
+      }
+      const response = await fetch(endpoint, options);
+      const data = await response.json();
+
+      dispatch(stopLoading());
+      return { data, response };
+    } catch (error) {
+      console.log(error);
+      return { error };
+    } finally {
+      dispatch(stopLoading());
+    }
+  };
+
+  return { handleFetch };
+};
+
+export default useFetch;

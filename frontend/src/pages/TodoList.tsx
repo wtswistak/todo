43930659e2a849468331373bfb2a88c11b74f0dc
wojtek <wswistak@student.agh.edu.pl>
2023-12-13 16:24:44 +0100

    get todos from db func

--- frontend/src/pages/TodoList.tsx
+++ frontend/src/pages/TodoList.tsx
@@ -1,28 +1,97 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import List from "../components/TodoList/List";
 import ListHeader from "../components/TodoList/ListHeader";
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser } from "../state/userSlice";
+import useFetch from "../hooks/useFetch";
+import config from "../config";
+import Loader from "../components/Loader";
+import {
+  selectCompletedTodos,
+  selectTodos,
+  setTodos,
+} from "../state/todoSlice";
+import { getHeaders } from "../helpers/helper";
 
 const TodoList = () => {
   const [isTodoListVisible, setIsTodoListVisible] = useState<boolean>(true);
   const [isCompletedListVisible, setIsCompletedListVisible] =
     useState<boolean>(true);
 
+  const dispatch = useDispatch();
+  const { id, isLoading } = useSelector(selectUser);
+  const { handleFetch } = useFetch();
+  const todos = useSelector(selectTodos);
+  const completedTodos = useSelector(selectCompletedTodos);
+  const isMounted = useRef(false);
+  const headers = getHeaders();
+
+  const getTodos = async () => {
+    const endpoint = config.SERVER_URL + `user/${id}/todos/`;
+
+    console.log(todos);
+    const { data } = await handleFetch(endpoint, "GET", headers);
+    dispatch(setTodos({ todos: data.todos }));
+  };
+  useEffect(() => {
+    if (!isMounted.current) {
+      isMounted.current = true;
+      getTodos();
+    }
+  }, [isMounted.current]);
+
   return (
-    <div className="container-fluid p-0 pe-sm-2  list-box">
-      <ListHeader
-        children={"Tasks to do"}
-        isVisible={isTodoListVisible}
-        setIsVisible={setIsTodoListVisible}
-      />
-      <List type={"todo"} isVisible={isTodoListVisible} />
-      <ListHeader
-        children={"Tasks completed"}
-        isVisible={isCompletedListVisible}
-        setIsVisible={setIsCompletedListVisible}
-      />
-      <List type={"completed"} isVisible={isCompletedListVisible} />
+    <div className="container-fluid p-0 pe-sm-2 list-box">
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <ListHeader
+            children={"Tasks todo"}
+            isVisible={isTodoListVisible}
+            setIsVisible={setIsTodoListVisible}
+          />
+          <List todos={todos} isVisible={isTodoListVisible} />
+          <ListHeader
+            children={"Tasks completed"}
+            isVisible={isCompletedListVisible}
+            setIsVisible={setIsCompletedListVisible}
+          />
+          <List todos={completedTodos} isVisible={isCompletedListVisible} />
+        </>
+      )}
     </div>
   );
 };
 
 export default TodoList;
+// const TodoList = () => {
+//   const [isTodoListVisible, setIsTodoListVisible] = useState<boolean>(true);
+//   const [isCompletedListVisible, setIsCompletedListVisible] =
+//     useState<boolean>(true);
+
+//   const [todos, setTodos] = useState<ITodo[]>([]);
+//   const [completedTodos, setCompletedTodos] = useState<ITodo[]>([]);
+//   const { handleFetch } = useFetch();
+//   const { id, isLoading } = useSelector(selectUser);
+//   const isMounted = useRef(false);
+
+//   const getTodos = async () => {
+//     const token = localStorage.getItem("token");
+//     const endpoint = config.SERVER_URL + `user/${id}/todos/`;
+
+//     const headers = {
+//       Authorization: `Bearer ${token}`,
+//     };
+
+//     const { data } = await handleFetch(endpoint, "GET", headers);
+//     setTodos(data.todos.filter((todo: ITodo) => !todo.completed));
+//     setCompletedTodos(data.todos.filter((todo: ITodo) => todo.completed));
+//     console.log("wys");
+//   };
+//   useEffect(() => {
+//     if (!isMounted.current) {
+//       isMounted.current = true;
+//       getTodos();
+//     }
+//   }, [isMounted.current]);

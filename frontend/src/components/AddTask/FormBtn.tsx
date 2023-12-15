08ba8541258601b70deba39b80d01e843dd65285
wojtek <wswistak@student.agh.edu.pl>
2023-12-15 22:02:20 +0100

    change props

--- frontend/src/components/AddTask/FormBtn.tsx
+++ frontend/src/components/AddTask/FormBtn.tsx
@@ -2,16 +2,14 @@ import Button from "react-bootstrap/Button";
 import Loader from "../Loader";
 
 interface FormBtnProps {
-  handleAddTask: () => void;
   isLoading?: boolean;
 }
 
-const FormBtn: React.FC<FormBtnProps> = ({ handleAddTask, isLoading }) => {
+const FormBtn: React.FC<FormBtnProps> = ({ isLoading }) => {
   return (
     <Button
       type="submit"
       className="form-btn rounded-4 px-4 py-2 mt-4 mb-1"
-      onClick={handleAddTask}
       disabled={isLoading}
     >
       {isLoading ? <Loader /> : "Add task"}

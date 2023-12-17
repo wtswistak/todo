6469067b054546ba0a6f83eb99b7de7aeac576d6
wojtek <wswistak@student.agh.edu.pl>
2023-12-17 20:01:40 +0100

    handle delete, update todo

--- frontend/src/components/TodoList/ListItem.tsx
+++ frontend/src/components/TodoList/ListItem.tsx
@@ -1,7 +1,28 @@
 import React from "react";
 import { IoMdTrash, IoMdCheckboxOutline } from "react-icons/io";
+import useFetch from "../../hooks/useFetch";
+import config from "../../config";
+import { useDispatch, useSelector } from "react-redux";
+import { removeTodo, updateTodo } from "../../state/todoSlice";
+import { getHeaders } from "../../helpers/helper";
 
 const ListItem: React.FC<{ todo: ITodo }> = ({ todo }) => {
+  const { handleFetch } = useFetch();
+  const todoId = todo.id;
+  const dispatch = useDispatch();
+  const headers = getHeaders();
+  const userId = localStorage.getItem("userId");
+  const endpoint = config.SERVER_URL + `user/${userId}/todos/${todoId}`;
+  const handleDeleteTodo = async () => {
+    await handleFetch(endpoint, "DELETE", headers);
+    dispatch(removeTodo(todo.id));
+  };
+
+  const handleUpdateTodo = async () => {
+    await handleFetch(endpoint, "PATCH", headers);
+    dispatch(updateTodo(todo.id));
+  };
+
   return (
     <li className="task-box d-flex py-2 px-3 rounded-1 align-items-center justify-content-between ">
       <div className="d-flex align-items-center">
@@ -17,13 +38,19 @@ const ListItem: React.FC<{ todo: ITodo }> = ({ todo }) => {
         <p>{todo.title}</p>
       </div>
       <div className="d-flex gap-1 align-items-center">
-        <IoMdTrash className="task-icon" size={26} title="delete" />
+        <IoMdTrash
+          className="task-icon"
+          size={26}
+          title="delete"
+          onClick={handleDeleteTodo}
+        />
 
         {!todo.completed && (
           <IoMdCheckboxOutline
             className="task-icon"
             size={26}
             title="complete"
+            onClick={handleUpdateTodo}
           />
         )}
       </div>

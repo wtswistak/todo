7ae1d35535607736e36ff68346b99676dde71162
wojtek <wswistak@student.agh.edu.pl>
2023-12-18 22:43:51 +0100

    delete user state

--- frontend/src/state/store.ts
+++ frontend/src/state/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./userSlice";
 import todoReducer from "./todoSlice";
 
 const store = configureStore({
   reducer: {
-    user: userReducer,
     todos: todoReducer,
   },
 });
--- frontend/src/state/userSlice.ts
+++ frontend/src/state/userSlice.ts
@@ -1,43 +1,37 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+// import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-interface UserState {
-  id: number | null;
-  username: string | null;
-  isLoading: boolean;
-}
+// interface UserState {
+//   id: number | null;
+//   username: string | null;
+//   isLoading: boolean;
+// }
 
-const initialState: UserState = {
-  id: null,
-  username: null,
-  isLoading: false,
-};
+// const initialState: UserState = {
+//   id: null,
+//   username: null,
+//   isLoading: false,
+// };
 
-const userSlice = createSlice({
-  name: "user",
-  initialState,
-  reducers: {
-    setUser: (
-      state,
-      action: PayloadAction<{ id: number; username: string }>
-    ) => {
-      state.id = action.payload.id;
-      state.username = action.payload.username;
-    },
-    clearUser: (state) => {
-      state.id = null;
-      state.username = null;
-    },
-    startLoading: (state) => {
-      state.isLoading = true;
-    },
-    stopLoading: (state) => {
-      state.isLoading = false;
-    },
-  },
-});
+// const userSlice = createSlice({
+//   name: "user",
+//   initialState,
+//   reducers: {
+//     setUser: (
+//       state,
+//       action: PayloadAction<{ id: number; username: string }>
+//     ) => {
+//       state.id = action.payload.id;
+//       state.username = action.payload.username;
+//     },
+//     clearUser: (state) => {
+//       state.id = null;
+//       state.username = null;
+//     },
+//   },
+// });
 
-export default userSlice.reducer;
-export const { setUser, clearUser, startLoading, stopLoading } =
-  userSlice.actions;
+// export default userSlice.reducer;
+// export const { setUser, clearUser, startLoading, stopLoading } =
+//   userSlice.actions;
 
-export const selectUser = (state: { user: UserState }) => state.user;
+// export const selectUser = (state: { user: UserState }) => state.user;

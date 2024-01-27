4b922df30cd187ecc3493c83a5c6c853130c3d6b
wojtek <wswistak@student.agh.edu.pl>
2024-01-27 14:12:18 +0100

    add local url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,6 @@
 const config = {
-  SERVER_URL: "https://todo-backend-w.vercel.app/api/v1/",
+  // SERVER_URL: "https://todo-backend-w.vercel.app/api/v1/",
+  SERVER_URL: "http://localhost:3000/api/v1/",
 };
 
 export default config;
